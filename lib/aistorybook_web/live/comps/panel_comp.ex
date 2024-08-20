1301135377a15dc12597efdd4621be3f429967f0
TomBers <tjcberman@gmail.com>
2024-08-20 20:20:18 +0100

    allow edit of panel

--- lib/aistorybook_web/live/comps/panel_comp.ex
+++ lib/aistorybook_web/live/comps/panel_comp.ex
@@ -1,13 +1,27 @@
 defmodule AistorybookWeb.Comps.PanelComp do
   use Phoenix.LiveComponent
+  import AistorybookWeb.CoreComponents
+  alias Phoenix.LiveView.JS
 
   def update(assigns, socket) do
-    # IO.inspect(assigns.panel, label: "Panel")
-    # img = assigns.panel.images |> List.first() |> then(& &1.url)
+    form =
+      assigns.panel
+      |> AshPhoenix.Form.for_update(:update, forms: [auto?: true])
+      |> to_form()
 
-    {:ok,
-     assign(socket,
-       panel: assigns.panel
-     )}
+    form_div_id = "update_panel_form_id_#{assigns.panel.id}"
+
+    {:ok, assign(socket, panel: assigns.panel, form: form, form_div_id: form_div_id)}
+  end
+
+  def handle_event("change_panel", %{"form" => params}, socket) do
+    {:ok, panel} = AshPhoenix.Form.submit(socket.assigns.form, params: params)
+
+    form =
+      panel
+      |> AshPhoenix.Form.for_update(:update, forms: [auto?: true])
+      |> to_form()
+
+    {:noreply, assign(socket, panel: panel, form: form)}
   end
 end
