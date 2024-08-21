36f2b881e04f00596630e3b4ea851830714998ce
TomBers <tjcberman@gmail.com>
2024-08-21 20:53:46 +0100

    draw text with box

--- assets/js/graphics.js
+++ assets/js/graphics.js
@@ -1,3 +1,10 @@
+// Potential params
+// Font
+// Font size
+// Text Color
+// Rect Color
+// Text Position 9 pos, left, center, right * top, middle, bottom
+
 export default function drawPanel({ text, canvasId, imgUrl }) {
   const canvas = document.getElementById(canvasId);
   const ctx = canvas.getContext("2d");
@@ -10,15 +17,31 @@ export default function drawPanel({ text, canvasId, imgUrl }) {
     // Draw the image on the canvas
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-    // Set text properties
-    ctx.font = "40px Arial";
-    ctx.fillStyle = "white";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
+    ctx.font = "40px Arial";
+
+    measure = ctx.measureText(text);
+
+    const padding = 15;
+    const width = measure.width + padding;
+    const height =
+      measure.actualBoundingBoxAscent +
+      measure.actualBoundingBoxDescent +
+      padding;
+
+    const x = canvas.width / 2; // Center horizontally
+    // const y = canvas.height / 2; // Center vertically
+    const y = canvas.height - height / 2;
+
+    ctx.rect(x - width / 2, y - height / 2, width, height);
+    ctx.fillStyle = "black";
+    ctx.fill();
+
+    // Set text properties
+    ctx.fillStyle = "white";
 
     // Position and draw the text over the image
-    const x = canvas.width / 2; // Center horizontally
-    const y = canvas.height / 2; // Center vertically
     ctx.fillText(text, x, y);
   };
 }
