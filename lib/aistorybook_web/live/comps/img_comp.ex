282b2baecbfe3ed01736e5d798b580528c78234d
TomBers <tjcberman@gmail.com>
2024-08-22 12:13:45 +0100

    remove unused imports

--- lib/aistorybook_web/live/comps/img_comp.ex
+++ lib/aistorybook_web/live/comps/img_comp.ex
@@ -1,8 +1,5 @@
 defmodule AistorybookWeb.Comps.ImgComp do
   use Phoenix.LiveComponent
-  import AistorybookWeb.CoreComponents
-  alias Phoenix.LiveView.JS
-  alias Aistorybook.Page.GenPanel
 
   def update(assigns, socket) do
     {:ok, assign(socket, panel: assigns.panel)}
