fe6fc0bcfd4ae943b74e5739a4b3564a5ea074cf
TomBers <tjcberman@gmail.com>
2024-08-22 15:11:39 +0100

    fix text dragging issue

--- assets/js/graphics.js
+++ assets/js/graphics.js
@@ -1,16 +1,15 @@
 export default function drawPanel({ text, canvasId, imgUrl, textConfig }) {
-  console.log(textConfig);
   const tc = JSON.parse(textConfig);
 
   const canvas = document.getElementById(canvasId);
   const ctx = canvas.getContext("2d");
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   const img = new Image();
   img.src = imgUrl;
 
   // Draw the image and text on the canvas once the image has loaded
   img.onload = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     // Draw the image on the canvas
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
@@ -31,7 +30,9 @@ export default function drawPanel({ text, canvasId, imgUrl, textConfig }) {
     // const y = canvas.height / 2; // Center vertically
     const y = tc["y"];
 
+    ctx.beginPath();
     ctx.rect(x - width / 2, y - height / 2, width, height);
+    ctx.closePath();
     ctx.fillStyle = tc["background_col"];
     ctx.fill();
 
